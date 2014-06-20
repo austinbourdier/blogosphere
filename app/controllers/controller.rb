63ee509ab1338648e9a16b796c8792a21a75453b
Matias Meneses <matiasameneses@gmail.com>
2014-06-20 15:27:16 -0700

    finished controller

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -7,34 +7,69 @@ require_relative '../view/view'
 
 class Controller
   def self.run
-    if self.logged_in
-      response = View.logged_in_menu
-    else
-      response = View.logged_out_menu
-    end
+    logged_out_menu
+  end
+
+  def logged_out_menu
+    response = View.logged_out_menu
 
     case response
-    when "Register"
-    loop do
-      username = View.get_username
-      password = View.get_password
+    when "1" #register
+      loop do
+        username = View.get_username
+        password = View.get_password
+        if User.make_new_user(username,password) == false
+          puts "The user name already exists, try again"
+        end
+        break if User.make_new_user(username,password)
+      end
+    logged_out_menu
 
-      User.make_new_user(username,password)
+    when "2" #log-in
+      loop do
+        username = View.get_username
+        password = View.get_password
+        if User.check_password(username,password) ==  false
+          puts "Invaid password for username, try again!"
+        end
+        break if User.check_password(username,password)
+      end
+      User.set_logged_in_true(username)
+    logged_in_menu(username)
 
-
-
-
-
-
-    when "Log-in"
-
-    when "Open the page"
-
-    when "Log-out"
-
-    when "Exit"
+    when "3" #open page
+      exec 'ruby ../view/ruby_to_html.rb'
+    logged_out_menu
+    when "4" #exit
+      return
+    else
+      puts "Please choose a number from the list."
+    end
 
   end
 
-end
+  def logged_in_menu (username)
 
+      response = View.logged_in_menu
+
+      case response
+      when "1" # post
+        title = View.get_title
+        content = View.get_post
+        tag = View.get_tag
+        Post.add_post(username,title,content,tag)
+        "Your post is added!"
+      logged_in_menu(username)
+      when "2" #open page
+        exec 'ruby ../view/ruby_to_html.rb'
+      logged_in_menu(username)
+      when "3" #log out
+        puts "You are now logged out!"
+        User.set_loggen_in_false(username)
+      logged_out_menu
+      when "4" #exit
+        return
+      else
+        puts "please choose a number from the list"
+      end
+end
