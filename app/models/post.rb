d1a856358db91a78bd9bedec91838f62b3b33ed4
Kris Shanks <apprentice@dbc14.local>
2014-06-20 15:22:40 -0700

    updated post to list posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,24 +4,14 @@ class Post < ActiveRecord::Base
   has_many :tags
 
   def self.list_posts
-    posts = []
-
-
-    Post.all.each do |post|
-      data = []
-      tags = []
-      data[0] = post.title
-      data[1] = post.content
-      tag_objects = Tag.joins(:post).where("posts.id = ?", post.id)
-      tag_objects.each{|tag| tags << tag.tag}
-      data[2] = tags
-      posts << data
+    Post.all.map do |post|
+      [post.title, post.content, post.tags.pluck(:tag)]
     end
-   posts
   end
 
-  def self.add_post(title,content,*tags)
+  def self.add_post(username, title, content, *tags)
     post = Post.create(title: title, content: content)
+    user = User.
     tags.each do |tag|
       post.tags << Tag.new(tag: tag)
     end
