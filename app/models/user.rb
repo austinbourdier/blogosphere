e410a3856e669202aa5ef5d8af1ca664f5cc8305
Kris Shanks <apprentice@dbc14.local>
2014-06-20 14:23:05 -0700

    updated users and post - able to add posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,30 @@
 class User < ActiveRecord::Base
   has_many :posts
 
+  validates :username, uniqueness: true
 
+  def self.make_new_user(username, password)
+    if User.create(username: username).valid?
+      User.create(username: username, password: password, logged_in: false)
+    else
+      return false
+    end
+  end
+
+  def self.set_logged_in_true(username)
+    user = User.where('username = ?', username)
+    user.logged_in = true
+    save.user
+  end
+
+  def self.set_logged_in_false(username)
+    user = User.where('username = ?', username)
+    user.logged_in = false
+    save.user
+  end
+
+  def self.get_password(username, password)
+    user = User.where('username = ?', username)
+    user.password == password
+  end
 end
